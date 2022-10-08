c2b5d7e48fad8e44a502a5a5f2b050e6b6efc619
Gaurav_is_the_one <76547705+gauravkm59829@users.noreply.github.com>
2022-10-08 11:15:57 +0530

    Time: 1168 ms (12.14%), Space: 129.1 MB (87.21%) - LeetHub

--- 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.cpp
@@ -2,27 +2,17 @@ class Solution {
 public:
     vector<int> arrayChange(vector<int>& nums, vector<vector<int>>& operations) {
         int n = operations.size();
-        unordered_map<int,int> mp,vis,m;
-        
-        for(int i = 0; i < nums.size();i++)
-          mp[nums[i]] = 0;
-        for(int i = 0; i < n; i++){
-            
-            if(vis[operations[i][0]] == 1){
-                mp[m[operations[i][0]]] = operations[i][1];
-                m[operations[i][1]] = m[operations[i][0]];
-            } else {
-                mp[operations[i][0]] = operations[i][1];
-                m[operations[i][1]] = operations[i][0];
-            }
-           vis[operations[i][1]] = 1;
-          
-        }
+        unordered_map<int,int> mp;
         
         for(int i = 0; i < nums.size(); i++){
-            if(mp[nums[i]] != 0)
-                nums[i] = mp[nums[i]];
+            mp[nums[i]] = i;
         }
+        
+        for(int i = 0; i < n; i++){
+            nums[mp[operations[i][0]]] = operations[i][1];
+            mp[operations[i][1]] = mp[operations[i][0]];
+        }
+        
         return nums;
     }
 };
