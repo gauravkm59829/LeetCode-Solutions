4ea5a233a598170365138496391e7fa0cc442a2a
Gaurav_is_the_one <76547705+gauravkm59829@users.noreply.github.com>
2022-11-11 14:02:42 +0530

    Time: 673 ms (26.16%), Space: 161.8 MB (5.44%) - LeetHub

--- 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
+++ 1887-reduction-operations-to-make-the-array-elements-equal/1887-reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {
-        int n = nums.size(),op = 0;
-        sort(nums.begin(),nums.end());
+        int n = nums.size(),op = 0,prev = 0;
+        set<int> st;
         unordered_map<int,int> mp;
         int num = nums[0];
         
         for(auto it : nums){
             mp[it]++;
+            st.insert(it);
         }
-        for(int i = n - 1; i > 0; i--){
-            if(nums[i] != nums[i - 1] && nums[i] != num){
-                op += mp[nums[i]];
-                mp[nums[i - 1]] += mp[nums[i]];
-            }
+        auto it2 = st.end();
+        --it2;
+        while(true){
+           // cout << *(it2) << "\n";
+            if(it2 != st.begin()){
+                mp[(*it2)] += prev;
+                op += mp[*(it2)];
+                prev = mp[*(it2)];
+            } else 
+                break;
+           // cout << op << "\n";
+            it2--;
         }
         
         return op;
