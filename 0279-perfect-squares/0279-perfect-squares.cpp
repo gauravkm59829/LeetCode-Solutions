22d6c903414d0ebbd9e3105504c12246e4c13453
Gaurav_is_the_one <76547705+gauravkm59829@users.noreply.github.com>
2022-11-22 23:19:30 +0530

    Time: 389 ms (52.57%), Space: 151.4 MB (8.91%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int fun(int ind, int cap, vector<int>& arr , vector<vector<int>> &dp){
+        if(ind == 0){
+            if((cap % arr[ind]) == 0)  
+                return cap / arr[ind];
+            else return 1e9;
+        } 
+        if(dp[ind][cap] != -1){
+            return dp[ind][cap];
+        }
+        int take = INT_MAX;
+            if(arr[ind] <= cap)
+             take = 1 + fun(ind, cap - arr[ind] ,arr , dp);
+         int notTake =  fun(ind - 1 , cap , arr , dp);
+        return dp[ind][cap] = min(take , notTake);
+
+    }
+    int numSquares(int n) {
+        vector<int> sq;
+        for(int i = 1 ; i * i <= n ;i++){
+            sq.push_back(i*i);
+        }
+        vector<vector<int>> dp(sq.size() , vector<int>(n + 1 , -1));
+    
+        return fun(sq.size() - 1, n, sq, dp);    
+        
+    }
+};
